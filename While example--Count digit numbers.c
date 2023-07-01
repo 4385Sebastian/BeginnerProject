f84bba5d78b62c8ff11a7d90404a8c1212a99c42
4385Sebastian <134257669+4385Sebastian@users.noreply.github.com>
2023-07-01 16:07:01 +0800

    Add files via upload

---
+++ While example--Count digit numbers.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int integer = 0, digit = 0;
+	printf("Please input a postive integer with nine digits at most:");
+	scanf("%d", &integer);
+	while (integer > 0) {
+		integer /= 10;
+		digit++;
+	}
+	printf("The integer has %d digits.", digit);
+}
