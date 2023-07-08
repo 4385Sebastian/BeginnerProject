2c43ba1b20921bbff26ca89070d4ef71fe2832bd
4385Sebastian <134257669+4385Sebastian@users.noreply.github.com>
2023-07-08 17:10:46 +0800

    Add files via upload

---
+++ For & Sequence 2.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int n = 0, denominator = 1;
+	double f = 0.0;
+	printf("n=");
+	scanf("%d", &n);
+	for (; denominator <= n;)
+	{
+		if (denominator % 2 == 0) {
+			f -= 1.0 / denominator;
+		}
+		else
+		{
+			f += 1.0 / denominator;
+		}
+		denominator++;
+	}
+	printf("f(n)=%f", f);
+	return 0;
+}
