6c0317c016606db87ec07b0ee0671e4ccf93f45a
4385Sebastian <134257669+4385Sebastian@users.noreply.github.com>
2023-07-11 16:26:57 +0800

    Add files via upload

---
+++ Forever Loops Example.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+	while (true) //It's also allowed to use 1, without even haivng to include "stdbool.h".
+	{
+		printf("meow\n");
+	}
+}
