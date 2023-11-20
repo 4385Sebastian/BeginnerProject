9e4d98fa1c769bc9760c8bec08ac2ca50f8be02e
4385Sebastian <134257669+4385Sebastian@users.noreply.github.com>
2023-11-20 15:02:19 +0800

    Add files via upload

---
+++ CS50 - W3 - Recursion - Collatz - Self-designed.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int collatz(int n);
+
+int main()
+{
+	int n = 0;
+	printf("The Collatz conjecture applies to positive integers and speculates that it is always possible to get back to 1 if you follow some steps.\n");
+	do
+	{
+		printf("Input a positive integer: ");
+		scanf("%d", &n);
+	} while (n < 1);
+	int times = collatz(n);
+	printf("It took %d times to get back to 1.", times);
+}
+
+int collatz(int n)
+{
+	static int times = -1;
+	if (1 == n)
+	{
+		times++;
+	}
+	else if (n % 2 == 0)
+	{
+		times++;
+		collatz(n / 2.0);
+	}
+	else
+	{
+		times++;
+		collatz(3 * n + 1);
+	}
+	return times;
+}
